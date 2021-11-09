a1b2eab5517ecd8afa32d78e941449a13fed6d08
Christian Hadirahardja <chris.hadirahardja+github@gmail.com>
2021-11-09 18:42:28 +0700

    Move handlers out of main()

--- server.go
+++ server.go
@@ -24,6 +24,8 @@ type Config struct {
 	}
 }
 
+var pgOpts *pg.Options
+
 func main() {
 	var cfg Config
 	err := envconfig.Process("", &cfg)
@@ -31,7 +33,7 @@ func main() {
 		panic(err)
 	}
 
-	pgOpts := &pg.Options{
+	pgOpts = &pg.Options{
 		Network:  cfg.Pg.Network,
 		Addr:     cfg.Pg.Addr,
 		Database: cfg.Pg.Database,
@@ -41,40 +43,48 @@ func main() {
 
 	e := echo.New()
 	e.Use(middleware.Gzip())
-	e.GET("/", func(ctx echo.Context) error {
-		tmpl := template.Must(template.ParseGlob("views/*.html"))
-		sb := &strings.Builder{}
-		tmpl.ExecuteTemplate(sb, "chart", nil)
-		return ctx.HTML(http.StatusOK, sb.String())
-	})
-	e.GET("symbols", func(ctx echo.Context) error {
-		db := pg.Connect(pgOpts)
-		repo := &pgStockSymbolRepository{db: db}
-		symbols, err := repo.get()
-		if err != nil {
-			e.Logger.Error(err)
-			return ctx.NoContent(http.StatusInternalServerError)
-		}
-		syms := make([]string, len(symbols))
-		for i, symbol := range symbols {
-			syms[i] = symbol.Symbol
-		}
-		return ctx.JSON(http.StatusOK, syms)
-	})
-	e.GET("/prices/:symbol", func(ctx echo.Context) error {
-		symbol := strings.ToUpper(ctx.Param("symbol")) + ".XIDX"
-		db := pg.Connect(pgOpts)
-		repo := &pgStockPriceRepository{db: db}
-		prices, err := repo.getBySymbol(symbol)
-		if err != nil {
-			e.Logger.Error(err)
-			return ctx.NoContent(http.StatusInternalServerError)
-		}
-		if prices == nil {
-			return ctx.NoContent(http.StatusNotFound)
-		}
-		return ctx.JSON(http.StatusOK, prices)
-	})
+	e.GET("/", getHome)
+	e.GET("symbols", getSymbols)
+	e.GET("/prices/:symbol", getPricesBySymbol)
 	e.Static("/static", "static")
 	e.Logger.Fatal(e.Start(":" + strconv.Itoa(cfg.Port)))
+}
+
+func getHome(ctx echo.Context) error {
+	tmpl := template.Must(template.ParseGlob("views/*.html"))
+	sb := &strings.Builder{}
+	tmpl.ExecuteTemplate(sb, "chart", nil)
+	return ctx.HTML(http.StatusOK, sb.String())
+}
+
+func getSymbols(ctx echo.Context) error {
+	db := pg.Connect(pgOpts)
+	defer db.Close()
+	repo := &pgStockSymbolRepository{db: db}
+	symbols, err := repo.get()
+	if err != nil {
+		ctx.Echo().Logger.Error(err)
+		return ctx.NoContent(http.StatusInternalServerError)
+	}
+	syms := make([]string, len(symbols))
+	for i, symbol := range symbols {
+		syms[i] = symbol.Symbol
+	}
+	return ctx.JSON(http.StatusOK, syms)
+}
+
+func getPricesBySymbol(ctx echo.Context) error {
+	symbol := strings.ToUpper(ctx.Param("symbol")) + ".XIDX"
+	db := pg.Connect(pgOpts)
+	defer db.Close()
+	repo := &pgStockPriceRepository{db: db}
+	prices, err := repo.getBySymbol(symbol)
+	if err != nil {
+		ctx.Echo().Logger.Error(err)
+		return ctx.NoContent(http.StatusInternalServerError)
+	}
+	if prices == nil {
+		return ctx.NoContent(http.StatusNotFound)
+	}
+	return ctx.JSON(http.StatusOK, prices)
 }
